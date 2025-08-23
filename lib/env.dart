e30461973173fa2b4674a4a9124bb756f6921133
sublime-developer-5 <developer5@sublimetechnocorp.com>
2025-08-23 11:20:41 +0530

    version changes

--- lib/env.dart
+++ lib/env.dart
@@ -1,8 +1,8 @@
 
 class EnvConfigs{
 
-  static const appBaseUrl ="http://13.127.244.215:5000/mobile/";
-  static const appBaseReg  ="http://13.127.244.215:5000/";
+  static const appBaseUrl ="http://176.57.189.224:6000/mobile/";
+  static const appBaseReg  ="http://176.57.189.224:6000/";
 
   // static const appBaseUrl ="http://192.168.0.117:4000/mobile/";
   // static const appBaseReg  ="http://192.168.0.117:4000/";
